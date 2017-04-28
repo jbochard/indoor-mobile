84e96830ec52459a4a9d6a2e4d5e6e27d0aa30dd
Juan Pablo Bochard <jbochard@despegar.com>
2017-04-28 16:51:28 -0300

    Se mejora interfaz de sensores

--- src/pages/sensors/sensors.ts
+++ src/pages/sensors/sensors.ts
@@ -1,6 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
 import { IndoorController } from "../../providers/indoor-controller";
+import { Observable, Subscription } from 'rxjs/Rx';
 
 /**
  * Generated class for the Sensors page.
@@ -12,11 +13,16 @@ import { IndoorController } from "../../providers/indoor-controller";
   selector: 'page-sensors',
   templateUrl: 'sensors.html',
 })
-export class SensorsPage {
+export class SensorsPage implements OnDestroy {
 
+  private timer;
+  private sub: Subscription;
+
+  values: Map<string, any>;
   sensors: Array<Array<any>>;
   relays: Array<Array<any>>;
   columns = 2;
+  showDisabled = true;
 
   constructor(
     public navCtrl: NavController, 
@@ -24,10 +30,12 @@ export class SensorsPage {
     public indoorController: IndoorController) {
         this.sensors = Array<Array<any>>();
         this.relays = Array<Array<any>>();
+        this.values = new Map<string, any>();
 
-        this.indoorController
-          .readSensors(this.navParams.data.ip)
-          .then((result) => this.buildSensors(result));
+        this.buildGrid(this.showDisabled);
+
+        this.timer = Observable.timer(2000,5000);
+        this.sub = this.timer.subscribe(t => this.updateSensors());
    }
 
    sensor_subtitle(sensor: any) {
@@ -42,27 +50,54 @@ export class SensorsPage {
      }
   }
 
-   private buildSensors(data: Array<any>) {
-      let i = 0;
-      let j = 0;
-      let row_sensor = Array<any>();
-      let row_relay = Array<any>();
-      data.forEach(d => {
-        if (d.type != "RELAY") {
-          if (i % this.columns == 0) {
-            row_sensor = Array<any>();
-            this.sensors.push(row_sensor);
+  private updateSensors() {
+    this.indoorController
+      .readSensors(this.navParams.data.ip)
+      .then((data) => {
+        data.forEach(d => {
+          if (this.values.has(d.port+d.type)) {
+            this.values.get(d.port+d.type).value = d.value;
           }
-          row_sensor.push(d);
-          i = i + 1;
-        } else {
-          if (j % this.columns == 0) {
-            row_relay = Array<any>();
-            this.relays.push(row_relay);
-          }
-          row_relay.push(d);
-          j = j + 1;          
-        }
+        });
+    });      
+  }
+
+  private buildGrid(showDisabled: boolean) {
+    this.indoorController
+      .readSensors(this.navParams.data.ip)
+      .then((data) => {
+          let i = 0;
+          let j = 0;
+          let lst_sensors = Array<Array<any>>();
+          let lst_relays =  Array<Array<any>>();
+          let row_sensor = Array<any>();
+          let row_relay = Array<any>();
+          data.forEach(d => {
+            if (showDisabled || (!showDisabled && d.enable)) {
+              if (d.type != "RELAY") {
+                if (i % this.columns == 0) {
+                  row_sensor = Array<any>();
+                  lst_sensors.push(row_sensor);
+                }
+                row_sensor.push(d);
+                i = i + 1;
+              } else {
+                if (j % this.columns == 0) {
+                  row_relay = Array<any>();
+                  lst_relays.push(row_relay);
+                }
+                row_relay.push(d);
+                j = j + 1;          
+              }
+              this.values.set(d.port+d.type, d);
+            }
+          });
+          this.sensors = lst_sensors;
+          this.relays = lst_relays;
       });
-   }
+  }
+
+  ngOnDestroy(){
+    this.sub.unsubscribe();
+  }
 }
--- src/providers/indoor-controller.ts
+++ src/providers/indoor-controller.ts
@@ -11,69 +11,74 @@ import 'rxjs/add/operator/map';
 @Injectable()
 export class IndoorController {
 
-  constructor(public http: Http) {
-  }
+    constructor(public http: Http) {
+    }
 
-  readSensors(host: string): Promise<Array<any>> {
-      let data = Array<any>();
-      data.push({
-                "name": "Temperatura",
-                "type": "DHT22_TEMP",
-                "port": 2,
-                "enable": false,
-                "manual": false,
-                "value": 27
-            });
-      data.push({
-                "name": "Humedad",
-                "type": "DHT22_HUM",
-                "port": 2,
-                "enable": false,
-                "manual": false,
-                "value": 65
-            });
-      data.push({
-                "name": "Relay1",
-                "type": "RELAY",
-                "port": 14,
-                "enable": false,
-                "manual": false,
-                "value": 0
-            });
-      data.push({
-                "name": "Relay2",
-                "type": "RELAY",
-                "port": 12,
-                "enable": false,
-                "manual": false,
-                "value": 0
-            });
-      data.push({
-                "name": "Relay3",
-                "type": "RELAY",
-                "port": 13,
-                "enable": false,
-                "manual": false,
-                "value": 0
-            });
-      data.push({
-                "name": "Relay4",
-                "type": "RELAY",
-                "port": 15,
-                "enable": false,
-                "manual": false,
-                "value": 0
-            });
-      data.push({
-                "name": "SoilMoisture1",
-                "type": "SOIL_MOISTURE",
-                "port": 16,
-                "enable": false,
-                "manual": false,
-                "value": 0
-            });
-      return new Promise((resolve, reject) => {
-        resolve(data);
-      });
-  }
+    readSensors(host: string): Promise<Array<any>> {
+        let data = Array<any>();
+        data.push({
+            "name": "Temperatura",
+            "type": "DHT22_TEMP",
+            "port": 2,
+            "enable": false,
+            "manual": false,
+            "value": this.getRandomInt(20, 30)
+        });
+        data.push({
+            "name": "Humedad",
+            "type": "DHT22_HUM",
+            "port": 2,
+            "enable": false,
+            "manual": false,
+            "value": this.getRandomInt(60, 70)
+        });
+        data.push({
+            "name": "Relay1",
+            "type": "RELAY",
+            "port": 14,
+            "enable": false,
+            "manual": false,
+            "value": 0
+        });
+        data.push({
+            "name": "Relay2",
+            "type": "RELAY",
+            "port": 12,
+            "enable": false,
+            "manual": false,
+            "value": 0
+        });
+        data.push({
+            "name": "Relay3",
+            "type": "RELAY",
+            "port": 13,
+            "enable": false,
+            "manual": false,
+            "value": 0
+        });
+        data.push({
+            "name": "Relay4",
+            "type": "RELAY",
+            "port": 15,
+            "enable": false,
+            "manual": false,
+            "value": 0
+        });
+        data.push({
+            "name": "SoilMoisture1",
+            "type": "SOIL_MOISTURE",
+            "port": 16,
+            "enable": false,
+            "manual": false,
+            "value": 0
+        });
+        return new Promise((resolve, reject) => {
+            resolve(data);
+        });
+    }
+
+   private getRandomInt(min: number, max: number) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
 }
