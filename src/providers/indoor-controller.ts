fdb67dc1617e2c9058ffd88d735b229f2aee89aa
Juan Pablo Bochard <jbochard@gmail.com>
2017-05-01 11:02:02 -0300

    Se agrega mock de resupesta del resto de los servicios del controller

--- src/providers/indoor-controller.ts
+++ src/providers/indoor-controller.ts
@@ -95,6 +95,51 @@ export class IndoorController {
         });
     }
 
+    readClock(host: string): Promise<Array<any>> {
+        let data = Array<any>();
+        data.push({
+            "year": 2017,
+            "month": 5,
+            "day": 1,
+            "hour": 10,
+            "minute": 21,
+            "second": 10
+        });
+        return new Promise((resolve, reject) => {
+            resolve(data);
+        });
+    }
+
+    // http://192.168.1.5/hardware/clock?year=2017&month=3&day=13&hour=19
+    writeClock(host: string): Promise<any> {
+        return new Promise((resolve, reject) => {
+            resolve({ status: 'OK' });
+        });
+    }
+
+    readRules(host: string): Promise<Array<any>> {
+        let data = Array<any>();
+        data.push("if Temperatura >= 20 then on(Relay1)");
+        data.push("if  hora_actual >= 0 and hora_actual <= 18 then on(Relay1) else off(Relay1)");  
+        return new Promise((resolve, reject) => {
+            resolve(data);
+        });
+    }
+
+    // http://192.168.1.5/rules?rule=if  hora_actual >= 0 and hora_actual <= 18 then on(Relay1) else off(Relay1)
+    writeRule(host:string, rule: string): Promise<any> {
+        return new Promise((resolve, reject) => {
+            resolve({ status: 'OK' });
+        });
+    }
+
+    // http://192.168.1.5/rules?idx=1
+    deleteRule(host:string, idx: number): Promise<any> {
+        return new Promise((resolve, reject) => {
+            resolve({ status: 'OK' });
+        });
+    }
+
     private getRandomInt(min: number, max: number) {
         return Math.floor(Math.random() * (max - min + 1)) + min;
     }
